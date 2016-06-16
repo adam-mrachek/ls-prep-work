9a51c8065f0d6228ee579734c77d87c1164c9eba
Adam Mrachek <amrachek@gmail.com>
2016-06-16 07:37:47 -0600

    Updating hashes exercise 6 solution

--- hashes/exercise_6.rb
+++ hashes/exercise_6.rb
@@ -2,8 +2,15 @@ words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
           'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
           'flow', 'neon']
 
-words.each do |n|
-  if n == n.sort
-    puts n
+anagrams = {}
+
+words.each do |word|
+  key = word.split("").sort.join
+  if anagrams.has_key?(key)
+    anagrams[key] << word
+  else
+    anagrams[key] = [word]
   end
 end
+
+anagrams.each { |k, v| p v}
