a3bdb9a1c68c59e7b9dbfdc1202de445ffcce657
Adam Mrachek <amrachek@gmail.com>
2016-06-22 07:18:13 -0600

    Adding the final Intro to Programming end-of-book exercise solutions

---
+++ end_exercises/exercise_17.rb
@@ -0,0 +1,10 @@
+# This program will puts 'These hashes are the same!' since they are the same hashes using different styles.
+
+hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
+hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
+
+if hash1 == hash2
+  puts "These hashes are the same!"
+else
+  puts "These hashes are not the same!"
+end
